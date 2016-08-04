f921602fbffb228cf9241813ee6edde44dd9fc0e
dafnelpz <iktan.da@gmail.com>
2016-08-03 20:30:19 -0500

    cambié algo en migración add devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :confirmable, :timeoutable, :omniauthable, omniauth_providers: [:facebook]
   belongs_to :role
   has_and_belongs_to_many :user
   has_many :agents, foreign_key: :agent_id
