112c88a1734132c920c890a131c8e491359519bb
dafnelpz <iktan.da@gmail.com>
2016-08-04 22:06:40 -0500

    un login más cool

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,21 @@
 module ApplicationHelper
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+  
+  def resource_class
+    devise_mapping.to
+  end
+  
+  def devise_mapping
+    Devise.mappings[:user]
+  end
+  
 	def flash_message
     flash_collection = []
 	    if flash[:notice]
