2e2eefb0816365df172e4dd3537aceaf3b4e13b1
dafnelpz <iktan.da@gmail.com>
2016-10-02 22:40:14 -0500

    modificando flash  messages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -16,7 +16,7 @@ class Users::SessionsController < DeviseController
   # POST /resource/sign_in
   def create
     self.resource = warden.authenticate!(auth_options)
-    set_flash_message!(:notice, :signed_in)
+    # set_flash_message!(:notice, :signed_in)
     # sign_in(resource_name, resource)
     yield resource if block_given?
     if resource.role.admin or resource.role.agent
@@ -29,7 +29,7 @@ class Users::SessionsController < DeviseController
   # DELETE /resource/sign_out
   def destroy
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
-    set_flash_message! :notice, :signed_out if signed_out
+    # set_flash_message! :notice, :signed_out if signed_out
     yield if block_given?
     respond_to_on_destroy
   end
