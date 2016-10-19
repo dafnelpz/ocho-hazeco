a316001fd7f348cf54d642919143bff6d2e3d1a5
dafnelpz <iktan.da@gmail.com>
2016-10-18 19:13:50 -0500

    intentando actualizar la info del usuario

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
 	def authenticate_admin_user!
 		unless signed_in? and (current_user.role.admin or current_user.role.agent)
@@ -14,4 +15,11 @@ class ApplicationController < ActionController::Base
 		flash[:error] = "Sin acceso"
 		redirect_to root_path
 	end
+
+	protected
+
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :phone, :address, :password, :password_confirmation])
+		end
+
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+  before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
@@ -42,20 +42,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
     resource.update_without_password(params)
   end
 
-  def update_sanitized_params
-    devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:name, :email, :password, :password_confirmation)}
-    devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:name, :email, :password, :password_confirmation, :current_password)}
-  end
-
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
   #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+    def configure_account_update_params
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :phone, :address, :password, :password_confirmation])
+    end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
